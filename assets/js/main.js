73784cef733f8ea1bf0b743f62301ac099eae075
johnnycistudent <johnoco18@gmail.com>
2020-07-02 12:21:54 +0000

    js updated to smooth scroll to elements from nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
+$(document).ready(function() {
+    // Scrolls smoothly to About section when the Navbar "About Me" link is pressed
+    $('#about-me-link').click(function() {
+        $('html, body').animate({ scrollTop: $('.about-section').offset().top }, 1000);
+    });
 
-let aboutScroll = document.querySelector('.about-section');
+    // Scrolls smoothly to Portfolio section when the Navbar "Portfolio" link is pressed
+    $('#portfolio-link').click(function() {
+        $('html, body').animate({ scrollTop: $('.portfolio-section').offset().top }, 1000);
+    });
+    // Scrolls smoothly to Contact section when the Navbar "Contact" link is pressed
+    $('.contact-me').click(function() {
+        $('html, body').animate({ scrollTop: $('.contact-section').offset().top }, 1000);
+    });
 
-aboutScroll.scrollIntoView();
-
+});
